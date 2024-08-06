463c98abd5689f6ffde2c9ed23000bb2215bd736
ZeMuA <48907268+zemua@users.noreply.github.com>
2024-08-06 23:22:05 +0200

    i18n

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const RewardRavenApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class RewardRavenApp extends StatelessWidget {
+  const RewardRavenApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Reward Raven',
+      localizationsDelegates: [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: [
+        Locale('en'), // English
+        // Locale('es'), // Spanish
+      ],
+      title: AppLocalizations.of(context)!.appTitle,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
