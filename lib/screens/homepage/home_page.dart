dc9e7dd07bc4224fe5a681ccaf3d36ef0cba7c65
ZeMuA <48907268+zemua@users.noreply.github.com>
2024-08-11 13:01:21 +0200

    home screen dialogs

---
+++ lib/screens/homepage/home_page.dart
@@ -0,0 +1,152 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(AppLocalizations.of(context)!.homePage),
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          child: IntrinsicWidth(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      child: ElevatedButton(
+                        onPressed: () {
+                          print('Positive apps button pressed');
+                        },
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(Icons.thumb_up),
+                            SizedBox(width: 8),
+                            Text(AppLocalizations.of(context)!.positiveApps),
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: 10),
+                    Expanded(
+                      child: ElevatedButton(
+                        onPressed: () {
+                          print('Positive groups button pressed');
+                        },
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(Icons.group),
+                            SizedBox(width: 8),
+                            Text(AppLocalizations.of(context)!.positiveGroups),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      child: ElevatedButton(
+                        onPressed: () {
+                          print('Negative apps button pressed');
+                        },
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(Icons.thumb_down),
+                            SizedBox(width: 8),
+                            Text(AppLocalizations.of(context)!.negativeApps),
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: 10),
+                    Expanded(
+                      child: ElevatedButton(
+                        onPressed: () {
+                          print('Negative groups button pressed');
+                        },
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(Icons.group_off),
+                            SizedBox(width: 8),
+                            Text(AppLocalizations.of(context)!.negativeGroups),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20),
+                Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: () {
+                        print('Random checks button pressed');
+                      },
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(Icons.shuffle),
+                          SizedBox(width: 8),
+                          Text(AppLocalizations.of(context)!.randomChecks),
+                        ],
+                      ),
+                    ),
+                  ),
+                ]),
+                SizedBox(height: 20),
+                Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: () {
+                        print('My Times button pressed');
+                      },
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(Icons.access_time),
+                          SizedBox(width: 8),
+                          Text(AppLocalizations.of(context)!.myTimings),
+                        ],
+                      ),
+                    ),
+                  ),
+                ]),
+                SizedBox(height: 20),
+                Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: () {
+                        print('Settings button pressed');
+                      },
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(Icons.settings),
+                          SizedBox(width: 8),
+                          Text(AppLocalizations.of(context)!.settings),
+                        ],
+                      ),
+                    ),
+                  ),
+                ]),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
