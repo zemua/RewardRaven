75d4502a3efee78e1a83093136238372b8d3932c
ZeMuA <48907268+zemua@users.noreply.github.com>
2024-08-15 14:15:07 +0200

    filter out current app from app list

--- lib/screens/apps/fetcher/impl/android_apps_fetcher.dart
+++ lib/screens/apps/fetcher/impl/android_apps_fetcher.dart
@@ -1,10 +1,16 @@
 import 'package:installed_apps/app_info.dart';
 import 'package:installed_apps/installed_apps.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 import 'package:reward_raven/screens/apps/fetcher/apps_fetcher.dart';
 
 class AndroidAppsFetcher implements AppsFetcher {
   @override
-  Future<List<AppInfo>> fetchInstalledApps() {
-    return InstalledApps.getInstalledApps();
+  Future<List<AppInfo>> fetchInstalledApps() async {
+    List<AppInfo> apps = await InstalledApps.getInstalledApps();
+
+    PackageInfo packageInfo = await PackageInfo.fromPlatform();
+    String currentPackageName = packageInfo.packageName;
+
+    return apps.where((app) => app.packageName != currentPackageName).toList();
   }
 }
