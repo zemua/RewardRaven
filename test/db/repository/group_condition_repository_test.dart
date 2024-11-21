c9170f1be929fc3349d3ec96cba08e672d8b6e3e
ZeMuA <48907268+zemua@users.noreply.github.com>
2024-11-21 22:18:03 +0100

    group conditions WIP

--- test/db/repository/group_condition_repository_test.dart
+++ test/db/repository/group_condition_repository_test.dart
@@ -2,8 +2,9 @@ import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
+import 'package:reward_raven/db/entity/group_condition.dart';
 import 'package:reward_raven/db/helper/firebase_helper.dart';
-import 'package:reward_raven/db/repository/listed_app_repository.dart';
+import 'package:reward_raven/db/repository/group_condition_repository.dart';
 import 'package:reward_raven/main.dart';
 
 import 'app_group_repository_test.mocks.dart';
@@ -13,12 +14,57 @@ void main() {
   final MockFirebaseHelper mockFirebaseHelper = MockFirebaseHelper();
   locator.registerSingleton<FirebaseHelper>(mockFirebaseHelper);
   late MockDatabaseReference mockDatabaseReference;
-  late ListedAppRepository listedAppRepository;
+  late GroupConditionRepository groupConditionRepository;
 
   setUp(() {
     mockDatabaseReference = MockDatabaseReference();
     when(mockFirebaseHelper.databaseReference)
         .thenAnswer((_) async => mockDatabaseReference);
-    listedAppRepository = ListedAppRepository();
+    groupConditionRepository = GroupConditionRepository();
+  });
+
+  test('saveGroupCondition saves a group condition successfully', () async {
+    // Arrange
+    const groupCondition = GroupCondition(
+      conditionedGroupId: 'testConditionedGroupId',
+      conditionalGroupId: 'testConditionalGroupId',
+      usedTime: Duration(hours: 1),
+      duringLastDays: 7,
+    );
+    when(mockDatabaseReference.child(any)).thenReturn(mockDatabaseReference);
+    when(mockDatabaseReference.set(any)).thenAnswer((_) => Future.value());
+    when(mockDatabaseReference.path).thenReturn('testPath');
+
+    // Act
+    await groupConditionRepository.saveGroupCondition(groupCondition);
+
+    // Assert
+    verify(mockDatabaseReference.child('groupConditions')).called(1);
+    verify(mockDatabaseReference.child('testConditionedGroupId')).called(1);
+    verify(mockDatabaseReference.child('testConditionalGroupId')).called(1);
+    verify(mockDatabaseReference.set(groupCondition.toJson())).called(1);
+  });
+
+  test('updateGroupCondition updates a group condition successfully', () async {
+    // Arrange
+    const groupCondition = GroupCondition(
+      conditionedGroupId: 'testConditionedGroupId',
+      conditionalGroupId: 'testConditionalGroupId',
+      usedTime: Duration(hours: 1),
+      duringLastDays: 7,
+    );
+    when(mockDatabaseReference.child(any)).thenReturn(mockDatabaseReference);
+    when(mockDatabaseReference.update(any)).thenAnswer((_) => Future.value());
+
+    // Act
+    await groupConditionRepository.updateGroupCondition(groupCondition);
+
+    // Assert
+    verify(mockDatabaseReference.child('groupConditions')).called(1);
+    verify(mockDatabaseReference.child('testConditionedGroupId')).called(1);
+    verify(mockDatabaseReference.child('testConditionalGroupId')).called(1);
+    verify(mockDatabaseReference.update(groupCondition.toJson())).called(1);
+
+    fail("Not implemented");
   });
 }
