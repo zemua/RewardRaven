1171d48ce519159241a7b6aab23240160d6eccab
ZeMuA <48907268+zemua@users.noreply.github.com>
2025-05-11 18:32:29 +0200

    implementing native app checker

--- android/app/src/main/java/dev/mrp/reward_raven/MainActivity.java
+++ android/app/src/main/java/dev/mrp/reward_raven/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity;
 import io.flutter.embedding.engine.FlutterEngine;
 import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugin.common.StandardMethodCodec;
 
 public class MainActivity extends FlutterActivity {
     private static final String CHANNEL = "mrp.dev/appinfo";
@@ -11,15 +12,24 @@ public class MainActivity extends FlutterActivity {
     @Override
     public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
         super.configureFlutterEngine(flutterEngine);
-        new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL)
-                .setMethodCallHandler(
-                        (call, result) -> {
-                            if (call.method.equals("getForegroundAppInfo")) {
-                                String foregroundAppInfo = getForegroundAppInfo();
-                                result.success(foregroundAppInfo);
-                            }
-                        }
-                );
+
+        MethodChannel channel = new MethodChannel(
+                flutterEngine.getDartExecutor().getBinaryMessenger(),
+                CHANNEL,
+                StandardMethodCodec.INSTANCE,
+                flutterEngine.getDartExecutor().getBinaryMessenger().makeBackgroundTaskQueue()
+        );
+
+        channel.setMethodCallHandler(
+                (call, result) -> {
+                    if (call.method.equals("getForegroundAppInfo")) {
+                        String foregroundAppInfo = getForegroundAppInfo();
+                        result.success(foregroundAppInfo);
+                    } else {
+                        result.notImplemented();
+                    }
+                }
+        );
     }
 
     private String getForegroundAppInfo() {
